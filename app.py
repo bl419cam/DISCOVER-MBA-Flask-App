fb067b31711f1c742e0d91ba49ee5dc009683731
bl419cam <bopingliu88@gmail.com>
2019-10-11 11:52:59 -0700

    institute waitress

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def results():
    return render_template("results.html", edu_hist=edu_hist, exp_hist=exp_hist)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=5000)
